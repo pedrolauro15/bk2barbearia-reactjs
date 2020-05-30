e361422f670fd5df6fd7735e0d2f04314b5559ea
unknown <pedrolauro15@gmail.com>
2020-05-29 23:29:02 -0300

    Add header

--- src/components/homeHeader/index.js
+++ src/components/homeHeader/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './styles.css';
 
+import { FaHome  } from 'react-icons/fa';
+import { GiBeard } from 'react-icons/gi';
+import { GoMegaphone } from 'react-icons/go';
+import { FiInstagram, FiLogIn, FiPhone } from 'react-icons/fi';
+
+//https://react-icons.github.io/react-icons/
+
 import assets from '../../assets';
 
 function HomeHeader() {
+  function changeSelected(e){
+
+    document.querySelector('.home-header-menu ul li.selected').classList.remove('selected');
+    e.target.classList.add('selected');
+  }
+
   return(
     <div id="home-header">
       <img alt="logo.png" src={assets.carinha_branca}/>
 
       <div className="home-header-menu">
 	      <ul className="home-header-menu-ul">
-          <li><a href="/./">Home</a></li>
-          <li>BK2 Barbearia</li>
-          <li>Promoções</li>
-          <li>Redes Sociais</li>
-          <li>Contato</li>
-          <li>Login</li>
+          <li className="selected" onClick={e => changeSelected(e)}><FaHome  className="header-icon"/><a className="" href="/./">HOME</a></li>
+          <li onClick={e => changeSelected(e)}><GiBeard className="header-icon"/> BK2 BARBEARIA</li>
+          <li onClick={e => changeSelected(e)}><GoMegaphone className="header-icon"/> PROMOÇÕES</li>
+          <li onClick={e => changeSelected(e)}><FiInstagram className="header-icon"/> REDES SOCIAIS</li>
+          <li onClick={e => changeSelected(e)}><FiPhone className="header-icon"/> CONTATOS</li>
+          <li onClick={e => changeSelected(e)}><FiLogIn className="header-icon"/> LOGIN</li>
         </ul>
       </div>
     </div>
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles.css';
 
 import HomeHeader from '../../components/homeHeader';
+import Carosel from '../../components/carosel';
 
 function Homepage() {
   return(
     <div id="homepage">
       <HomeHeader />
+      <Carosel />
     </div>
     
   );
